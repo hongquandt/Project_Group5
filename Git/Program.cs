a6972d2a7fd560be86a29776b85b4b7d572d5edc
hongquandt <160511165+hongquandt@users.noreply.github.com>
2025-09-23 09:14:21 +0700

    implement add

---
+++ Git/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("====== Simple Calculator ======");
+                Console.WriteLine("1. Cộng");
+                Console.WriteLine("2. Trừ");
+                Console.WriteLine("3. Nhân");
+                Console.WriteLine("4. Chia");
+                Console.WriteLine("0. Thoát");
+                Console.Write("Chọn chức năng: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Add();
+                        break;
+                    case 2:
+                      
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        
+                        break;
+                    case 0:
+                        Console.WriteLine("Thoát chương trình.");
+                        break;
+                    default:
+                        Console.WriteLine("Lựa chọn không hợp lệ!");
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (choice != 0);
+        }
+
+        static void Add()
+        {
+            Console.Write("Nhập số thứ nhất: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhập số thứ hai: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Kết quả: {a} + {b} = {a + b}");
+        }
+
+        
+    }
+}
